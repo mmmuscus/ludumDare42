2e924d4333d89cf4dadf712abd5c170b6a35ebe0
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-08-11 22:05:10 +0200

    pre first build version (menu added)

---
+++ ludumDare42/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+	private bool MouseIn = false;
+	private bool MouseClick = false;
+
+	void OnMouseEnter()
+	{
+		MouseIn = true;
+	}
+
+	void OnMouseLeave()
+	{
+		MouseIn = false;
+	}
+
+	void OnMouseDown()
+	{
+		MouseClick = true;
+	}
+
+	void OnMouseUp()
+	{
+		MouseClick = false;
+	}
+
+	void Update () 
+	{
+		if (MouseIn && MouseClick)
+		{
+			SceneManager.LoadScene("Week");
+		}
+	}
+}
