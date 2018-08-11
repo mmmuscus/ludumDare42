a25b485e00ddef0f9e198244965bab9bc3cd0042
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-08-11 06:52:15 +0200

    Drag 'n' Drop functional

---
+++ ludumDare42/Assets/Scripts/DragScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour
+{
+    private bool dragging = false;
+    private float distance;
+ 
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+ 
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+ 
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }
+    }
+}
