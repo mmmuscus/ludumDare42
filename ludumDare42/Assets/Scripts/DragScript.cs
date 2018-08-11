8be1d9504c2039240b7b928786e685ba6ddebc1a
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-08-11 10:02:31 +0200

    OverFlow is working

--- ludumDare42/Assets/Scripts/DragScript.cs
+++ ludumDare42/Assets/Scripts/DragScript.cs
@@ -6,16 +6,56 @@ public class DragScript : MonoBehaviour
 {
     private bool dragging = false;
     private float distance;
- 
+
+	public float StartXPosition = -5f;
+	public float StartYPosition = 0f;
+
+	private GameObject[] OFB;
+	private Collider2D[] OFBC;
+	private Collider2D ThisCollider;
+
+	public void SetBackToSpawnPosition()
+	{
+		transform.position = new Vector3 (StartXPosition, StartYPosition, 0f);
+	}
+
+	void Start()
+	{
+		ThisCollider = this.GetComponent<Collider2D>();
+
+		if (OFB == null)
+		{
+			OFB = GameObject.FindGameObjectsWithTag("OFB");
+		}
+
+		OFBC = new Collider2D[OFB.Length];
+
+		for (int i = 0; i < OFB.Length; i++)
+		{
+			OFBC[i] = OFB[i].GetComponent<Collider2D>();
+		}
+	}
+
     void OnMouseDown()
     {
         distance = Vector3.Distance(transform.position, Camera.main.transform.position);
-        dragging = true;
+		dragging = true;
+		ThisCollider.isTrigger = true;
     }
  
     void OnMouseUp()
     {
         dragging = false;
+
+		foreach (Collider2D other in OFBC)
+		{
+			if (ThisCollider.IsTouching(other))
+			{
+				SetBackToSpawnPosition();
+			}
+		}
+
+		ThisCollider.isTrigger = false;
     }
  
     void Update()
