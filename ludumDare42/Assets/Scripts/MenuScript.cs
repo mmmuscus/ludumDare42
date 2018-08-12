54825bfd375d7718525d6d354c68aa6773cd8193
mmmuscus <32553610+mmmuscus@users.noreply.github.com>
2018-08-12 16:36:34 +0200

    ohhh....okay....

--- ludumDare42/Assets/Scripts/MenuScript.cs
+++ ludumDare42/Assets/Scripts/MenuScript.cs
@@ -5,34 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-	private bool MouseIn = false;
-	private bool MouseClick = false;
-
 	void OnMouseEnter()
 	{
-		MouseIn = true;
-	}
-
-	void OnMouseLeave()
-	{
-		MouseIn = false;
+		
 	}
 
 	void OnMouseDown()
 	{
-		MouseClick = true;
-	}
-
-	void OnMouseUp()
-	{
-		MouseClick = false;
-	}
-
-	void Update () 
-	{
-		if (MouseIn && MouseClick)
-		{
-			SceneManager.LoadScene("Week");
-		}
+		SceneManager.LoadScene("Week");
 	}
 }
